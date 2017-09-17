fa763d19ab4b03e346ef8eec6d15097eb0173938
yongting1 <15195893003@163.com>
2017-09-17 13:00:28 +0800

    xx

--- firmware_ascii/application/main.c
+++ firmware_ascii/application/main.c
@@ -41,7 +41,7 @@ the 1ms period.
 
 void main(void)
 {
-  G_u32SystemFlags |= _SYSTEM_INITIALIZING;
+      G_u32SystemFlags |= _SYSTEM_INITIALIZING;
 
   /* Low level initialization */
   WatchDogSetup(); /* During development, does not reset processor if timeout */
